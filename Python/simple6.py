1c468f4246049c8eb47b40f9ca6b55415418476c
Patryk Wójtowicz <106282113+UltiPro@users.noreply.github.com>
2023-02-25 22:10:49 +0100

    Update

--- Python/simple6.py
+++ Python/simple6.py
@@ -1,13 +1,27 @@
 class DivideByZeroException(Exception):
+    message = "źle lol"
     pass
+
 
 try:
     x = 5
     y = 0
-    if(y == 0) raise: DivideByZeroException
-except DivideByZeroException:
-    print("Nie dzielimy przez zero")
+    if (y == 0):
+        raise DivideByZeroException("tekst")
+    if (x == 5):
+        raise Exception()
+except DivideByZeroException as e:
+    print("Nie dzielimy przez zero. "+e.message)
 except Exception:
     print("coś")
 finally:
-    print("Koniec try'a")
+    print("Koniec try'a - finally")
+# except:  # nie robimy tego nie wszystko warto obsługiwać
+#    print("dla wszystkich innych")
+
+
+### stringi nie mogą być edytowane poprzez index ###
+
+string = "dupa"
+
+# string[2] = 'c' ### ERROR
