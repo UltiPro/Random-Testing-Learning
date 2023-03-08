3a5fe1e81113f9814852375b405866824260b4e3
Patryk Wójtowicz <106282113+UltiPro@users.noreply.github.com>
2023-03-08 23:51:36 +0100

    Update

--- Python Django/months/challenges/urls.py
+++ Python Django/months/challenges/urls.py
@@ -5,6 +5,7 @@ from . import views
 urlpatterns = [
     #path("january", views.january),
     #path("february", views.february),
+    path("", views.index),
     path("<int:month>", views.monthly_challange_by_number),
     path("<str:month>", views.monthly_challange, name="month-challenge")
 ]
--- Python Django/months/challenges/views.py
+++ Python Django/months/challenges/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect
 from django.urls import reverse
+# from django.template.loader import render_to_string
 
 # Create your views here.
 
@@ -8,7 +9,7 @@ month_list = {
     "january": "Hello january",
     "february": "Hello february",
     "march": "Hello march",
-    "may": "Hello may",
+    "may": None
 }
 
 
@@ -18,6 +19,21 @@ month_list = {
 
 # def february(request):
 #    return HttpResponse("Hello february!")
+
+def index(request):
+    list_items = ''
+    months = list(month_list.keys())
+
+    return render(request, "challenges/index.html", {
+        "months": months
+    })
+
+    '''for month in months:
+        cap_month = month.capitalize()
+        month_path = reverse("month-challenge", args=[month])
+        list_items += f"<li><a href=\"{month_path}\">{cap_month}</a></li>"
+
+    return HttpResponse(list_items)'''
 
 
 def monthly_challange(request, month):
@@ -30,7 +46,12 @@ def monthly_challange(request, month):
             return HttpResponseNotFound("Not supported")'''
     try:
         output = month_list[month]
-        return HttpResponse(output)
+        return render(request, "challenges/challenge.html", {  # dictionary
+            "output": output,
+            "month": month
+        })
+        # response = render_to_string("challenges/challenge.html")
+        # return HttpResponse(response)
     except:
         return HttpResponseNotFound("Not supported")
 
