063821294234859328535591bb54c612b7f6fa39
Patryk Wójtowicz <106282113+UltiPro@users.noreply.github.com>
2023-03-20 23:39:15 +0100

    Update

--- Python Django/feedback/reviews/forms.py
+++ Python Django/feedback/reviews/forms.py
@@ -1,7 +1,26 @@
 from django import forms
 
-class ReviewForm(forms.Form):
-    name = forms.CharField(min_length=3, max_length=100, label="name2323", error_messages={ # required = False/True
-        "required": "tak",
-        "max_length": "nie"
-    })
+from .models import Review
+
+#class ReviewForm(forms.Form):
+#    name = forms.CharField(min_length=3, max_length=100, label="name2323", error_messages={ # required = False/True
+#        "required": "tak",
+#        "max_length": "nie"
+#   })
+#    review_text = forms.CharField(label="tak 2", widget=forms.Textarea, max_length=1000)
+#    rating = forms.IntegerField(label="Yuor Rating", min_value=1, max_value=5)
+
+class ReviewForm(forms.ModelForm):
+    class Meta:
+        model = Review
+        #fields = ['user_name', 'review-text', 'rating']
+        fields = '__all__' # all fields
+        #exclude = ['user_name', 'review-text']
+        labels = {
+            "user_name": "dupa"
+        }
+        error_messages = {
+            "user_name": {
+                "required": "dupa2"
+            }
+        }
--- Python Django/feedback/reviews/views.py
+++ Python Django/feedback/reviews/views.py
@@ -1,20 +1,45 @@
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
+from django.views import View
 
 from .forms import ReviewForm
+from .models import Review
 
 # Create your views here.
 
 
-def review(request):
-    if request.method == "POST":
+class ReviewView(View):
+    def get(self, request):
+        form = ReviewForm()
+        return render(request, "reviews/review.html", {
+            "form": form
+        })
+
+    def post(self, request):
         form = ReviewForm(request.POST)
         if form.is_valid():
+            form.save()
+            return HttpResponseRedirect("/ty")
+        return render(request, "reviews/review.html", {
+            "form": form
+        })
+
+
+def review(request):
+    if request.method == "POST":
+        exisitng_data = Review.objects.get(pk=1)
+        form = ReviewForm(request.POST, instance=exisitng_data)
+        if form.is_valid():
             print(form.cleaned_data)
+            # review = Review(user_name = form.cleaned_data['user_name'],
+            #                review_text = form.cleaned_data['review_text'],
+            #                rating = form.cleaned_data['rating'])
+            # review.save()
+            form.save()
             return HttpResponseRedirect("/ty")
     else:
         form = ReviewForm()
-    
+
     return render(request, "reviews/review.html", {
         "form": form
     })
