18d11c45ba0305839cc94e95e3470e8babd4dd7d
Patryk Wójtowicz <106282113+UltiPro@users.noreply.github.com>
2023-05-13 19:41:14 +0200

    Wpf

---
+++ C# WPF/WpfControlsButton/WpfControlsButton/MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfControlsButton
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void myButton_Click(object sender, RoutedEventArgs e)
+        {
+            double myFontsize = myLabel.FontSize;
+            myLabel.FontSize = myFontsize + 1;
+        }
+
+        private void myButton_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            double myFontsize = myLabel.FontSize;
+            myLabel.FontSize = myFontsize - 1;
+        }
+
+        private void myButton_MouseEnter(object sender, MouseEventArgs e)
+        {
+            myButton.Foreground = Brushes.Yellow;
+        }
+
+        private void myButton_MouseLeave(object sender, MouseEventArgs e)
+        {
+            myButton.Foreground = Brushes.Red;
+        }
+    }
+}
