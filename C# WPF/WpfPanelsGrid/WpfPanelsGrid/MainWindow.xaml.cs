8e722b3df0a583a97ebf84fdac52759fcc360a9d
Patryk Wójtowicz <106282113+UltiPro@users.noreply.github.com>
2023-05-13 22:38:34 +0200

    Wpf

--- C# WPF/WpfPanelsGrid/WpfPanelsGrid/MainWindow.xaml.cs
+++ C# WPF/WpfPanelsGrid/WpfPanelsGrid/MainWindow.xaml.cs
@@ -13,7 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace WpfControlsExpander
+namespace WpfPanelsGrid
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
@@ -23,11 +23,6 @@ namespace WpfControlsExpander
         public MainWindow()
         {
             InitializeComponent();
-        }
-
-        private void Expander_Expanded(object sender, RoutedEventArgs e)
-        {
-            MessageBox.Show("tak");
         }
     }
 }
