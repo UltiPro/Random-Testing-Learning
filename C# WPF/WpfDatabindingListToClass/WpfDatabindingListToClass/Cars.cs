8a95fc57ac07b905cb07678aa641f0f04b71e67d
Patryk Wójtowicz <106282113+UltiPro@users.noreply.github.com>
2023-05-18 03:41:21 +0200

    WPF

---
+++ C# WPF/WpfDatabindingListToClass/WpfDatabindingListToClass/Cars.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfDatabindingListToClass
+{
+    class Cars
+    {
+        public static List<Car> GetCars()
+        {
+            return new List<Car>()
+            {
+                new Car(){Owner="Mike", CarType=CarType.Hatchback},
+                new Car(){Owner="Mike2", CarType=CarType.Sedan},
+                new Car(){Owner="Mike3", CarType=CarType.SUV}
+            }.ToList();
+        }
+    }
+}
