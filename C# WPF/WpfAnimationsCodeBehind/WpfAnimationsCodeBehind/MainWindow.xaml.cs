421add0a297aa0044bcbf558bf695b2f628fea6f
Patryk Wójtowicz <106282113+UltiPro@users.noreply.github.com>
2023-05-20 23:29:05 +0200

    Wpf

---
+++ C# WPF/WpfAnimationsCodeBehind/WpfAnimationsCodeBehind/MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfAnimationsCodeBehind
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void MainGrid_Loaded(object sender, RoutedEventArgs e)
+        {
+            DoubleAnimation doubleAnimation = new DoubleAnimation();
+            doubleAnimation.Duration = TimeSpan.FromSeconds(1.0d);
+            doubleAnimation.From = 0.0d;
+            doubleAnimation.To = 1.0d;
+            MainGrid.BeginAnimation(Grid.OpacityProperty, doubleAnimation);
+
+        }
+
+        private void MainGrid_MouseEnter(object sender, MouseEventArgs e)
+        {
+            DoubleAnimation doubleAnimation = new DoubleAnimation(1.0d,0.0d,TimeSpan.FromSeconds(1.0d));
+            MainGrid.BeginAnimation(Grid.OpacityProperty, doubleAnimation);
+        }
+    }
+}
