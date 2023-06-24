2c8023417d139db467a47e43509516f01e666bf7
Patryk Wójtowicz <106282113+UltiPro@users.noreply.github.com>
2023-06-24 23:48:45 +0200

    Update Program.cs

--- C#/Theards/Program.cs
+++ C#/Theards/Program.cs
@@ -5,7 +5,23 @@ Console.WriteLine("Hello, World!");
 
 Thread.Sleep(1000); // sleep method
 
-new Thread(() => {
+new Thread(() => { // new thread
     Console.WriteLine("Theard 1");
 }).Start();
+
+TaskCompletionSource<bool> taskCompletionSource = new TaskCompletionSource<bool>(); //task complete obj
+
+Thread thread = new Thread(() => {
+    Console.WriteLine($"Thread number: {Thread.CurrentThread.ManagedThreadId} started"); 
+    Thread.Sleep(1000);
+    taskCompletionSource.TrySetResult(true);
+    Console.WriteLine($"Thread number: {Thread.CurrentThread.ManagedThreadId} ended"); 
+});
+
+Console.WriteLine($"Thread number: {thread.ManagedThreadId}"); //id of thread
+thread.Start();
+
+var test = taskCompletionSource.Task.Result;
+
+Console.WriteLine(test);
 
