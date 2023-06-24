e1657fb9fd2fc81d0f69c5d2da00aa316b53e7b6
Patryk Wójtowicz <106282113+UltiPro@users.noreply.github.com>
2023-06-24 23:30:40 +0200

    C# theards

---
+++ C#/Theards/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Threading;
+
+Console.WriteLine("Hello, World!");
+
+Thread.Sleep(1000); // sleep method
+
+new Thread(() => {
+    Console.WriteLine("Theard 1");
+}).Start();
+
