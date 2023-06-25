74db4ad661dd57dd9bf6fe3bb95ee75bfacda627
Patryk Wójtowicz <106282113+UltiPro@users.noreply.github.com>
2023-06-26 00:41:57 +0200

    Update Program.cs

--- C#/Theards/Program.cs
+++ C#/Theards/Program.cs
@@ -2,7 +2,23 @@
 using System.Threading;
 
 Console.WriteLine("Hello, World!");
+/*
+Thread th1 = new Thread(() => {
+    Thread.Sleep(5000);
+    Console.WriteLine("Theard 1");
+});
 
+th1.Start();
+
+new Thread(()=>{
+    Thread.Sleep(2000);
+    Console.WriteLine("Theard 2");
+}).Start();
+
+th1.Join(1000);
+
+Console.WriteLine("end");
+*/
 Thread.Sleep(1000); // sleep method
 
 new Thread(() =>
@@ -15,7 +31,7 @@ TaskCompletionSource<bool> taskCompletionSource = new TaskCompletionSource<bool>
 Thread thread = new Thread(() =>
 {
     Console.WriteLine($"Thread number: {Thread.CurrentThread.ManagedThreadId} started");
-    Thread.Sleep(1000);
+    Thread.Sleep(10000);
     taskCompletionSource.TrySetResult(true);
     Console.WriteLine($"Thread number: {Thread.CurrentThread.ManagedThreadId} ended");
 });
