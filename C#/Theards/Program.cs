5a8d007935259494be9e7ba3ace73de1fe95d7bd
Patryk Wójtowicz <106282113+UltiPro@users.noreply.github.com>
2023-06-25 21:15:26 +0200

    C# theards

--- C#/Theards/Program.cs
+++ C#/Theards/Program.cs
@@ -5,17 +5,19 @@ Console.WriteLine("Hello, World!");
 
 Thread.Sleep(1000); // sleep method
 
-new Thread(() => { // new thread
+new Thread(() =>
+{ // new thread
     Console.WriteLine("Theard 1");
 }).Start();
 
 TaskCompletionSource<bool> taskCompletionSource = new TaskCompletionSource<bool>(); //task complete obj
 
-Thread thread = new Thread(() => {
-    Console.WriteLine($"Thread number: {Thread.CurrentThread.ManagedThreadId} started"); 
+Thread thread = new Thread(() =>
+{
+    Console.WriteLine($"Thread number: {Thread.CurrentThread.ManagedThreadId} started");
     Thread.Sleep(1000);
     taskCompletionSource.TrySetResult(true);
-    Console.WriteLine($"Thread number: {Thread.CurrentThread.ManagedThreadId} ended"); 
+    Console.WriteLine($"Thread number: {Thread.CurrentThread.ManagedThreadId} ended");
 });
 
 Console.WriteLine($"Thread number: {thread.ManagedThreadId}"); //id of thread
@@ -25,3 +27,26 @@ var test = taskCompletionSource.Task.Result;
 
 Console.WriteLine(test);
 
+// next part polls and background theard
+
+Enumerable.Range(0, 100).ToList().ForEach(e =>
+{
+    ThreadPool.QueueUserWorkItem((o) => // dopóki działa program
+    {
+        Console.WriteLine($"Thread number: {Thread.CurrentThread.ManagedThreadId} started");
+        Thread.Sleep(1000);
+        Console.WriteLine($"Thread number: {Thread.CurrentThread.ManagedThreadId} ended");
+    });
+    /*new Thread(() => {
+        Console.WriteLine($"Thread number: {Thread.CurrentThread.ManagedThreadId} started"); 
+        Thread.Sleep(1000);
+        Console.WriteLine($"Thread number: {Thread.CurrentThread.ManagedThreadId} ended"); 
+    }).Start();*/
+});
+
+new Thread(() =>
+{
+    Console.WriteLine($"Thread number: {Thread.CurrentThread.ManagedThreadId} background started");
+}){ IsBackground = true }.Start();
+
+Console.ReadLine();
---
+++ C#/ThreadsJoinIsAlive/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Threading;
+
+Console.WriteLine("Main thread started");
+
+Thread thread1 = new Thread(Thread1Fun);
+Thread thread2 = new Thread(Thread2Fun);
+
+thread1.Start();
+thread2.Start();
+
+// czeka na zakończenie wątku
+/*thread1.Join(); 
+Console.WriteLine("th1 ended");
+thread2.Join();
+Console.WriteLine("th2 ended");*/
+
+if (thread1.Join(1000)) // czeka w określonym czasie
+{
+    Console.WriteLine("Thread1Fun done");
+}
+else
+{
+    Console.WriteLine("Thread1Fun not done in 1 sec");
+}
+thread2.Join();
+Console.WriteLine("Thread2Fun done");
+
+if (thread1.IsAlive) // czy dalej pracuje wątek
+{
+    Console.WriteLine("Th1 is still doing stuff");
+}
+else
+{
+    Console.WriteLine("Th1 completed");
+}
+
+Console.WriteLine("Main thread ended");
+
+static void Thread1Fun()
+{
+    Console.WriteLine("th1 started");
+    Thread.Sleep(3000);
+    Console.WriteLine("th1 ended");
+}
+
+static void Thread2Fun()
+{
+    Console.WriteLine("th2 started");
+}
+
+int x = 69;
+
+Task.Run(()=>{ // async programing 
+    Console.WriteLine(Thread.CurrentThread.ManagedThreadId);
+    Console.WriteLine(x);
+});
