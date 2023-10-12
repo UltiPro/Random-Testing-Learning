2ba6c8c7418f9d1ca650ba9c7841db9fd89bb76f
Patryk Wójtowicz <106282113+UltiPro@users.noreply.github.com>
2023-10-12 13:05:05 +0200

    React

--- JavaScript React/Base/src/App.js
+++ JavaScript React/Base/src/App.js
@@ -1,4 +1,5 @@
 import logo from './logo.svg';
+import ExpenseItem from './components/ExpenseItem';
 import './App.css';
 
 function App() {
@@ -18,6 +19,7 @@ function App() {
           Learn React
         </a>
       </header>
+      <ExpenseItem></ExpenseItem>
     </div>
   );
 }
---
+++ JavaScript React/base/src/components/ExpenseItem.js
@@ -0,0 +1,7 @@
+function ExpenseItem() {
+    return (
+        <h2>Hello</h2>
+    );
+}
+
+export default ExpenseItem;
